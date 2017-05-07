b0ad73485173d1e4a533680e577d049e73aa46dc
Innectic <innectic@gmail.com>
2017-05-07 14:43:28 -0700

    Fix config usage
    Make the quotes all the same type
    
    Little bit of documentation

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-const express = require("express");
-const BeamClient = require("beam-client-node");
+const express = require('express');
+const BeamClient = require('beam-client-node');
 
-const config = require('config');
-const port = config.get('port') || 80;
-const scopes = config.get('scopes');
+const config = require('./config/config.json');
+const port = config.port || 80;
+const scopes = config.scopes;
 
 const portAddition = port !== 80 ? `:${port}` : '';
-const redirectUri = `${config.get('baseUrl')}${portAddition}/callback`;
+const redirectUri = `${config.baseUrl}${portAddition}/callback`;
 
 const app = express();
 
 function createClient() {
     const client = new BeamClient();
-    client.use("oauth", {
-        clientId: config.get('clientId'),
-        secret: config.get('clientSecret'),
+    // Supply the OAuth information to the client.
+    client.use('oauth', {
+        clientId: config.clientId,
+        secret: config.clientSecret
     });
     return client;
 }
 
 const client = createClient();
 
-app.get("/", (request, res) => {
-    res.redirect(client.getProvider().getRedirect(redirectUri, scopes));
+app.get('/', (request, reply) => {
+    // Redirect clients who visit `/` to the authorization page.
+    reply.redirect(client.getProvider().getRedirect(redirectUri, scopes));
 });
 
-app.get("/callback", (request, reply) => {
+app.get('/callback', (request, reply) => {
+    // Respond with either an error, or with the information the client can use to authorize with.
     const oauth = client.getProvider();
 
     oauth.attempt(redirectUri, request.query)
         .then(res => reply.json(oauth.getTokens()))
-        .catch(err => reply.json(err));
+        .catch(reply.json);
 });
 
 app.listen(port, () => {
